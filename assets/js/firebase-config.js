54d885d20a309cf5b696226315344ae4f6f21492
machristinefatimagino-alt <ma.christinefatima.gino@bisu.edu.ph>
2026-06-02 20:03:14 +0800

    r

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,20 @@
-// Replace the values below with your Firebase project's configuration.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDg8mI_xQv6NFItVURXYhTkTcxerMFX9so",
+  authDomain: "bcstr-6c048.firebaseapp.com",
+  projectId: "bcstr-6c048",
+  storageBucket: "bcstr-6c048.firebasestorage.app",
+  messagingSenderId: "718064182195",
+  appId: "1:718064182195:web:12eb82d583405da5f021c6",
+  measurementId: "G-RM7NEL96J3"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
